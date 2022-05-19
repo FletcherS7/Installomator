5cecf6278c2115d4db1bba203b45eeb19514970f
Fletcher Salesky <fletcher@salesky.dev>
2022-05-19 13:56:59 -0700

    Update wechat.sh

--- fragments/labels/wechat.sh
+++ fragments/labels/wechat.sh
@@ -1,6 +1,8 @@
 wechat)
     name="WeChat"
     type="dmg"
-    downloadURL="https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
+    archiveName="WeChatMac.dmg"
+    downloadURL="$(curl -fsLIXGET https://dldir1.qq.com/weixin/mac/WeChatMac.dmg | grep -o -i -E 'https.*' | cut -d\& -f1)"
+    curlOptions=( -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15" )
     expectedTeamID="5A4RE8SF68"
     ;;
