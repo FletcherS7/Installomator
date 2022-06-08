c52fe6ea23a4d48a3d5e34c0aed497fa59207ed4
Fletcher Salesky <fletcher@salesky.dev>
2022-06-08 00:19:06 -0700

    add wechat curl options

--- fragments/labels/wechat.sh
+++ fragments/labels/wechat.sh
@@ -2,7 +2,17 @@ wechat)
     name="WeChat"
     type="dmg"
     archiveName="WeChatMac.dmg"
-    downloadURL="$(curl -fsLIXGET https://dldir1.qq.com/weixin/mac/WeChatMac.dmg | grep -o -i -E 'https.*' | cut -d\& -f1)"
-    curlOptions=( -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15" )
+    downloadURL="https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
+    curlOptions=( -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+-H "Accept-Language: en-US,en;q=0.9"
+-H "Connection: keep-alive"
+-H "Sec-Fetch-Dest: document"
+-H "Sec-Fetch-Mode: navigate"
+-H "Sec-Fetch-Site: none"
+-H "Sec-Fetch-User: ?1"
+-H "sec-gpc: 1"
+-H "Upgrade-Insecure-Requests: 1"
+-H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15"
+--compressed )
     expectedTeamID="5A4RE8SF68"
     ;;
