4d7c682a45c1c157f6c099a77119a7162206a0ad
Fletcher Salesky <fletcher@salesky.dev>
2022-06-15 14:30:24 -0700

    Create adobepremiererush-update.sh

---
+++ fragments/labels/adobepremiererush-update.sh
@@ -0,0 +1,15 @@
+adobepremiererush-update)
+    # Requires Adobe Remote Update Manager: https://helpx.adobe.com/enterprise/using/using-remote-update-manager.html
+    name="Adobe Premiere Rush"
+    type="updateronly"
+    blockingProcesses=( "Adobe Premiere Rush" )
+    expectedTeamID="JQ525L2MZD"
+    targetDir="/Applications/$(ls /Applications/ | grep Adobe\ Premiere\ Rush)"
+    appName="Adobe Premiere Rush.app"
+    if [[ -x "/usr/local/bin/RemoteUpdateManager" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
+        printlog "Running RemoteUpdateManager --productVersions=RUSH --action=list"
+        "/usr/local/bin/RemoteUpdateManager" --productVersions=RUSH --action=list
+    fi
+    updateTool="/usr/local/bin/RemoteUpdateManager"
+    updateToolArguments=( --productVersions=RUSH --action=install )
+    ;;
