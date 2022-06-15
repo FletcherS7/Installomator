d5409b3680c79eb0ec09a8aae1fd158c2ff7ca08
Fletcher Salesky <fletcher@salesky.dev>
2022-06-15 14:43:33 -0700

    Update adobeanimate-update.sh

--- fragments/labels/adobeanimate-update.sh
+++ fragments/labels/adobeanimate-update.sh
@@ -1,10 +1,11 @@
 adobeanimate-update)
+    # Requires Adobe Remote Update Manager: https://helpx.adobe.com/enterprise/using/using-remote-update-manager.html
     name="Adobe Animate"
     type="updateronly"
-    blockingProcesses=( "Animate" )
     expectedTeamID="JQ525L2MZD"
     targetDir="/Applications/$(ls /Applications/ | grep Adobe\ Animate\ 20)"
     appName="$(ls "$targetDir" | grep -o "Adobe Animate 20[^']*\.app")"
+    blockingProcesses="$(echo $appName | sed 's/\.[^.]*$//')"
     if [[ -x "/usr/local/bin/RemoteUpdateManager" && $INSTALL != "force" && $DEBUG -eq 0 ]]; then
         printlog "Running RemoteUpdateManager --productVersions=FLPR --action=list"
         "/usr/local/bin/RemoteUpdateManager" --productVersions=FLPR --action=list
